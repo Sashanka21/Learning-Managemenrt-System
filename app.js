1f89ed04889527e80da2d5923cf0c9430a34f817
Sashanka21 <99025735+Sashanka21@users.noreply.github.com>
2022-03-22 20:59:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
    .then(result =>
    {   console.log(result)
        console.log("connected to database")
-       app.listen(5000)
+       app.listen(process.env.PORT || 5000)
     })
    .catch((err) => console.log(err));
 
